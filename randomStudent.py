bf63bdd83bf6343474827cb1268db0f1076f327b
grace-mao23 <gmao00@stuy.edu>
2019-09-10 14:53:08 -0400

    Tested code

--- randomStudent.py
+++ randomStudent.py
@@ -2,9 +2,13 @@ import random as rand
 
 def randomStudent(d):
     teams = list() # create new list
-    for key in d.keys():n # d.keys() list NOT indexable
+    for key in d.keys(): # d.keys() list NOT indexable
         teams.append(key)
     team = teams[rand.randint(0,len(teams)-1)] # select the random team
-    student = d[team][rand.randint(0,len(team)-1)] # select the random student from team
+    student = d[team][rand.randint(0,len(d)-1)] # select the random student from team
     print (student) # print it
     # return nothing
+
+KREWES = { 'orpheus':['Emily','Kevin'], 'rex':['Joey','Jackie'] }
+
+randomStudent(KREWES);
