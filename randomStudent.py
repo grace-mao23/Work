e5e06ca86a9025f94b0833b90c4855a8dfe5cec0
grace-mao23 <gmao00@stuy.edu>
2019-09-10 15:09:40 -0400

    Put in the complete KREWES dict

--- randomStudent.py
+++ randomStudent.py
@@ -5,10 +5,21 @@ def randomStudent(d):
     for key in d.keys(): # d.keys() list NOT indexable
         teams.append(key)
     team = teams[rand.randint(0,len(teams)-1)] # select the random team
-    student = d[team][rand.randint(0,len(d)-1)] # select the random student from team
+    student = d[team][rand.randint(0,len(team)-1)] # select the random student from team
     print (student) # print it
     # return nothing
 
-KREWES = { 'orpheus':['Emily','Kevin'], 'rex':['Joey','Jackie'] }
+KREWES = {'orpheus':['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany',
+            'Amanda', 'Junhee', 'Jackie', 'Tyler', 'Emory', 'Ivan', 'Elizabeth',
+            'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael',
+            'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+        'rex':['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo',
+                'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham',
+                'Michael', 'Matthew', 'Jionghao', 'Devin', 'David', 'Jacob', 'Will',
+                'Hannah', 'Alex'],
+        'endymion':['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason',
+                'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren',
+                'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry',
+                'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']}
 
 randomStudent(KREWES);
