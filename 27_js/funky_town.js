31e3618cd4927885b5804ec1c01e10e8640f7104
grace-mao23 <gmao00@stuy.edu>
2019-12-10 14:47:20 -0500

    JavaScript functions

---
+++ 27_js/funky_town.js
@@ -0,0 +1,40 @@
+var fact = function(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * fact(n-1);
+}
+
+var fibonacci = function(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n <= 2) {
+    return 1;
+  }
+  return fib(n-1) + fib(n-2);
+}
+
+var gcd = function(a, b) {
+  if (a <= 1) {
+    return a;
+  }
+  if (b <= 1) {
+    return b;
+  }
+  if (a === b) {
+    return a;
+  }
+  if (a > b) {
+    return gcd(a - b, b);
+  }
+  return gcd(a, b - a);
+}
+
+var students = ["Joe", "Jane", "Bob", "Mr. Mykolyk", "Grace", "Leia", "Jude", "Connor", "Sophie"];
+
+var randomStudent = function() {
+  var x = Math.random() * students.length;
+  x = Math.floor(x);
+  return students[x];
+}
