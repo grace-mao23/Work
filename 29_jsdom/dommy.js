f42a12c44d5c4366595dcfeaac31e40931cb5552
grace-mao23 <gmao00@stuy.edu>
2019-12-12 20:25:52 -0500

    Assignment finished

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -3,40 +3,72 @@
 // K29 -- Sequential Progression III
 // 2019-12-12
 
-/*Upon button push, add an element to the list.
-When the mouse goes over an item in the list, change the heading at the top to contain the text of the item.
-When the mouse is no longer over an item in the list, change the heading back to "Hello World!"
-When an item on the list is clicked, remove it from the DOM.*/
+//var b = document.getElementById("error");
+//b.addEventListener('click', function(e) { console.log(e) });
 
-var changeHeading = function(e) {
+var changeHeading = function(e) { // e is an event
   var h = document.getElementById("h");
-  h.innerHTML = e;
+  if (e.type == 'mouseover') {
+    h.innerHTML = e.target.innerHTML;
+  } else {
+    h.innerHTML = "Hello World!";
+  }
 };
 
 var removeItem = function(e) {
-  e.remove();
+  e.target.remove();
 };
 
 var lis = document.getElementsByTagName("li");
 
 for (var i = 0; i < lis.length; i++) {
-  lis[i].addEventListener('mouseover', function(lis[i]) { changeHeading(lis[i]) });
-  lis[i].addEventListener('mouseout', changeHeading("Hello World!"));
-  lis[i].addEventListener('click', function(lis[i]) { removeItem(lis[i]) });
+  lis[i].addEventListener('mouseover', changeHeading);
+  lis[i].addEventListener('mouseout', changeHeading);
+  lis[i].addEventListener('click', removeItem);
 };
-/*
+
 var addItem = function(e) {
   var list = document.getElementById("thelist");
   var item = document.createElement("li");
   item.innerHTML = "WORD";
-  lis[i].addEventListener('mouseover', changeHeading(lis[i].innerHTML));
-  lis[i].addEventListener('mouseout', changeHeading("Hello World!"));
-  lis[i].addEventListener('click', removeItem(lis[i]));
+  item.addEventListener('mouseover', changeHeading);
+  item.addEventListener('mouseout', changeHeading);
+  item.addEventListener('click', removeItem);
   list.appendChild(item);
 };
 
 var button = document.getElementById("b");
-button.addEventListener('click', addItem);*/
+button.addEventListener('click', addItem);
 
-var b = document.getElementById("error");
-b.addEventListener('click', function(e) { console.log(e) });
+var fib = function(n) {
+  if (n < 2) {
+    return 1;
+  } else {
+    return fib(n-1) + fib(n-2);
+  }
+};
+
+var fibNums = [];
+
+var addFib = function(e) {
+  console.log(e);
+  var list = document.getElementById("fiblist");
+  var item = document.createElement("li");
+  item.innerHTML = addFib2(e);
+  list.appendChild(item);
+};
+
+var addFib2 = function(e) {
+  console.log(e);
+  var ans;
+  if (fibNums.length < 2) { // if the length is less than 2, the statement in else won't work
+    ans = fib(fibNums.length);
+  } else { // append the next fibonacci number into the list
+    ans = fibNums[fibNums.length - 1] + fibNums[fibNums.length - 2];
+  }
+  fibNums.push(ans);
+  return ans;
+};
+
+var fb = document.getElementById("fb");
+fb.addEventListener('click', addFib);
