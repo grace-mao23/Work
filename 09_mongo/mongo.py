2eb9cf704ecdbf4f632772c4732f1b9fafab7ba2
grace-mao23 <gmao00@stuy.edu>
2020-02-26 00:08:23 -0500

    Started assignment

---
+++ 09_mongo/mongo.py
@@ -0,0 +1,12 @@
+import pymongo
+
+client = pymongo.MongoClient('localhost', 27017) # port 27017
+db = client['testdata'] # does not have to exist
+collection = db['restaurants']
+
+with open('dataset.json') as f:
+    file_data = json.load(f) # read in json file
+
+collection.insert_many(file_data)
+
+client.close() # at the very end !!!
