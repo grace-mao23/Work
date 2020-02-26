5a6f7d7246bb65e6d42e6895105ae1183d54257d
grace-mao23 <gmao00@stuy.edu>
2020-02-26 00:19:53 -0500

    huh

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -5,7 +5,7 @@ db = client['testdata'] # does not have to exist
 collection = db['restaurants']
 
 with open('dataset.json') as f:
-    file_data = json.load(f) # read in json file
+    file_data = json.load(f.read()) # read in json file
 
 collection.insert_many(file_data)
 
