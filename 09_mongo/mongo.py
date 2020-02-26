52dbe902429429de9f663124ecf19c55fb3a0e98
grace-mao23 <gmao00@stuy.edu>
2020-02-26 00:27:29 -0500

    first query

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -4,5 +4,12 @@ client = pymongo.MongoClient('localhost', 27017) # port 27017
 db = client['testdata'] # does not have to exist
 collection = db['restaurants']
 
+def borough(b):
+    query = { "borough": b }
+    results = collection.find(query)
+    for x in query:
+        print(x)
+
+borough("Manhattan")
 
 client.close() # at the very end !!!
