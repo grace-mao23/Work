29ed2bac4de4fa34d8bd1876ce232167f782c9cd
grace-mao23 <gmao00@stuy.edu>
2020-02-27 19:27:18 +0000

    grrr

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -21,10 +21,12 @@ def zipcode(z):
 
 def grade(z, g):
     for r in col.find({"zipcode": z }):
-        record = r['grades']
-        if (record[record.length - 1] == g) {
+        record = r['records']
+	print(record)
+	print(record[record.length-1])
+        if (record[record.length - 1] == g):
             pprint.pprint(r)
-        }
+
 
 ### TEST CASES
 #borough("Manhattan")
