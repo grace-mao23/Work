cf6ab1201ba6dbe5224cdf691fd73880b37099c1
grace-mao23 <gmao00@stuy.edu>
2020-03-03 14:58:36 -0500

    delete

--- 10_mongo/mongo.py
+++ 10_mongo/mongo.py
@@ -20,40 +20,6 @@ def name(a):
         if r['name'][0] == a:
             pprint.pprint(r)
 
-# given a zipcode, list out all the restaurants with that zipcode
-def zipcode(z):
-    for r in col.find({ "address.zipcode": z }):
-        pprint.pprint(r)
-
-# given a zipcode, list out all the restaurants
-# whose last grade matches the given grade
-def grade(z, g):
-    for r in col.find({ "address.zipcode": z }):
-       # print(r)
-        record = r['grades']
-       # print(record)
-    #    print(record[len(record)-1])
-        if (record[len(record)-1]['grade'] == g):
-            pprint.pprint(r)
-
-# given a zipcode, list out all the restaurants
-# with any history of a grade lower than the threshold given
-def thres(z, g):
-    for r in col.find({ "address.zipcode": z }):
-        record = r['grades']
-        for i in range(len(record)):
-            if (record[i]['grade'] > g):
-                pprint.pprint(r)
-
-def cor(x, y):
-    for r in col.find({}):
-        restx = r['address']['coord'][0]
-        resty = r['address']['coord'][1]
-        distance = abs(restx - x) + abs(resty - y)
-        #print(distance)
-        if (distance <= 0.01):
-            pprint.pprint(r)
-
 ### TEST CASES ###
 name("A")
 
