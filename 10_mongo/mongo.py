4143f57a96da975d88910ac9d3ef9c9a8f4ac83d
grace-mao23 <gmao00@stuy.edu>
2020-03-03 23:19:44 -0500

    typo

--- 10_mongo/mongo.py
+++ 10_mongo/mongo.py
@@ -22,7 +22,7 @@ def name(a):
 
 # given a mass, return meteorites with mass within 50 kg of that mass
 def mass(n):
-	for r in col.find({ 'mass': {$exists: true} }):
+	for r in col.find({ 'mass': {"$exists": true} }):
 		if abs(n - int(r['mass'])) <= 50:
 			pprint.pprint(r)
 
